4b7ca9b34309adb5f3b76e25f31e327cfa8e80a6
Pavel Shevnin <pshevnin49@gmail.com>
2019-12-01 16:35:26 +0100

    Registration change test

--- Controllers/registrace.php
+++ Controllers/registrace.php
@@ -16,21 +16,16 @@
         <h1>Registrace</h1>
         <form class="prihlaseni" method="post" action="check.php">                           <?php //echo htmlspecialchars($_SERVER["PHP_SELF"])?>
             Username: <input type="text" class="form-control" name="name"  id="name" value="<?php echo $name?>" placeholder="Napište username">
-            <span class="error">* <?php echo $nameErr;?></span>
             <br><br>
             Email: <input type="email" class="form-control" name="email" id="emal" value="<?php echo $email?>" placeholder="Napište email">
-            <span class="error">* <?php echo $emailErr;?></span>
             <br><br>
             Heslo: <input type="password" class="form-control" name="heslo" id="heslo" value="<?php echo $heslo?>" placeholder="Napište heslo">
-            <span class="error">* <?php echo $hesloErr;?></span>
             <br><br>
             Heslo znovu: <input type="password" class="form-control" name="heslo_znovu" id="heslo_znovu" value="<?php echo $heslo_znovu?>" placeholder="Zopakujte heslo">
-            <span class="error">* <?php echo $heslo_znovuErr;?></span>
             <br><br>
             <!--<input type="radio" name="role" value="admin">Admin -->
             <input type="radio" class="form-control" name="role" id="role" <?php if(isset($role) && $role == "autor") echo "checked";?> value="autor">Autor
             <input type="radio" class="form-control" name="role"  id="role" <?php if(isset($role) && $role == "recenzent") echo "checked";?> value="recenzent">Recenzent
-            <span class="error">* <?php echo $roleErr;?></span>
             <br><br>
             <button class="btn btn-success" type="submit" >Zaregistrovat</button><br>
         </form>
