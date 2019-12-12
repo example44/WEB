6e76cd3915dba2c019bf4773249ebe57e478be22
example44 <simonovyan1999@gmail.com>
2019-12-12 15:31:52 +0100

    container

--- Views/TemplateBasics.class.php
+++ Views/TemplateBasics.class.php
@@ -23,7 +23,7 @@ class TemplateBasics {
                 <title><?php echo $pageTitle; ?></title>
             </head>
             <body>
-
+        <div class="container mt-4">
                 <div class="navbar navbar-expand-lg navbar-dark " style="background-color:black " >
 
                         <img src="logo2.jpg" width="150" height="60" alt="logo">
@@ -51,7 +51,7 @@ class TemplateBasics {
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-        <h1> <?php echo $pageTitle; ?></h1>
+            <h1> <?php echo $pageTitle; ?></h1>
 
         <?php
         }
@@ -65,6 +65,7 @@ class TemplateBasics {
                         <div class="footer-copyright text-center py-3"style="color: aliceblue">© 2019 Copyright:
                         </div>
                     </footer>
+            </div>
                 </body>
             </html>
             <?php
